d05f01e8d3b26ccba7a52663152799709b7fc769
simple <30411833+saifalhaneefa@users.noreply.github.com>
2026-09-22 17:37:55 +0530

    Export portfolio backtester

--- src/vcp_strategy/__init__.py
+++ src/vcp_strategy/__init__.py
@@ -2,5 +2,15 @@ from .config import VCPConfig, load_config
 from .indicators import add_indicators
 from .signal import VCPDetector
 from .backtest import Backtester
+from .portfolio import PortfolioBacktester, PortfolioResult, PortfolioTrade
 
-__all__ = ["VCPConfig","load_config","add_indicators","VCPDetector","Backtester"]
+__all__ = [
+    "VCPConfig",
+    "load_config",
+    "add_indicators",
+    "VCPDetector",
+    "Backtester",
+    "PortfolioBacktester",
+    "PortfolioResult",
+    "PortfolioTrade",
+]
